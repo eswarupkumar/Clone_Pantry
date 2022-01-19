204d7b45f3784ff97a289317a119d7c6379a0b3a
E Swarup Kumar <eswarupkumar1111@gmail.com>
2022-01-19 19:24:08 +0530

    Integrated all api functionalities

---
+++ models/table.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TableSchema = new Schema({
+  _id: {
+    type: Number,
+    default: Math.round(Math.random() * 10000),
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    default: "",
+    required: false,
+  },
+  box: [{}],
+  createdAt: {
+    type: Number,
+  },
+  updatedAt: {
+    type: Number,
+  },
+});
+
+const tableStorage = mongoose.model("Table", TableSchema);
+
+module.exports = tableStorage;
--- routes.js
+++ routes.js
@@ -1,9 +1,113 @@
 var express = require("express");
+const table = require("./models/table");
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  // res.status(200).json({ msg: "Api request recieved" });
-  res.send("<h1>Hello World</h1>");
+//Creating a new table
+router.post("/table/create/:tableName", async (req, res) => {
+  const { tableName } = req.params;
+  await table
+    .create({
+      name: tableName,
+      box: {},
+    })
+    .then((data) => {
+      res
+        .status(200)
+        .json({ Message: `New table create successfully with id ${data._id}` });
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to create the table. Please try again !");
+    });
+});
+
+//Get details of the table.
+router.get("/table/getTable/:tableId", async (req, res) => {
+  const { tableId } = req.params;
+  await table
+    .findById(tableId)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to find any table with the given id!");
+    });
+});
+
+//Creating a box inside the table
+router.post("/table/:tableId/box/:boxName", async (req, res) => {
+  const { tableId, boxName } = req.params;
+  const pushBox = { name: boxName };
+  if (Object.keys(req.query).length !== 0) {
+    for (var i = 0; i < Object.keys(req.query).length; i++) {
+      let key = Object.keys(req.query)[i];
+      let value = req.query[key];
+      pushBox[key] = value;
+    }
+  }
+  await table
+    .findByIdAndUpdate(tableId, { $push: { box: pushBox } })
+    .then((data) => {
+      res.status(200).json(`Your table was updated with box: ${boxName}`);
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to create box!");
+    });
+});
+
+//Get detials of the box
+router.get("/table/:tableId/box/:boxName", async (req, res) => {
+  const { tableId, boxName } = req.params;
+  await table
+    .findById(tableId)
+    .then((data) => {
+      const boxes = data["box"];
+      const obj = boxes.filter((o) => o.name === boxName);
+      res.status(200).json(obj);
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to create box!");
+    });
+});
+
+//Delete a box from the table
+router.delete("/table/:tableId/box/:boxName", async (req, res) => {
+  const { tableId, boxName } = req.params;
+  await table
+    .findByIdAndUpdate(tableId, { $pull: { box: { name: boxName } } })
+    .then((data) => {
+      res.status(200).json(`Box deleted successfully !`);
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to delete box!");
+    });
+});
+
+// Update the content of the box
+router.put("/table/:tableId/box/:boxName", async (req, res) => {
+  const { tableId, boxName } = req.params;
+  const key = Object.keys(req.query)[0];
+  const value = req.query[key];
+  await table
+    .findById(tableId)
+    .then((data) => {
+      const boxes = data["box"];
+      for (var i = 0; i < boxes.length; i++) {
+        if (boxes[i].name == boxName) {
+          boxes[i][key] = value;
+        }
+      }
+      table
+        .findByIdAndUpdate(tableId, { box: boxes })
+        .then((data) => {
+          res.status(200).json("Updated");
+        })
+        .catch((err) => {
+          res.status(400).json("Failed to update");
+        });
+    })
+    .catch((err) => {
+      res.status(400).json("Unable to find box!");
+    });
 });
 
 module.exports = router;
